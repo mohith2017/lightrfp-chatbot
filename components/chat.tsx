a7813ba9d0c46bf30218ac99ddd9035cb79331e3
mohith2017 <mohith.m.venkat@gmail.com>
2025-01-21 02:10:41 -0500

    Added LightRFP logo image

--- components/chat.tsx
+++ components/chat.tsx
@@ -67,7 +67,7 @@ export function Chat({
           selectedVisibilityType={selectedVisibilityType}
           isReadonly={isReadonly}
         /> */}
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center h-screen">
           <img src="/images/lightrfp-logo.png" alt="Chat Logo" className="w-32 h-32" />
         </div>
 
